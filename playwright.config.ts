b8d1cb4288c1560ea348de62830c6b4e11a12930
Gustavo dos Anjos <lalodosanjos@gmail.com>
2025-02-14 16:41:41 -0300

    fix(playwright): use environment-based headless config for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: !!process.env.CI,
     screenshot: "on",
     video: "on",
     /* Base URL to use in actions like `await page.goto('/')`. */
